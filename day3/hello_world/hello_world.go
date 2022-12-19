662dae19d46cb47dfdec594da727a6f994b778ac
Manigandan N <manigandan.n@everest.engineering>
2022-12-19 11:13:04 +0530

    #003 | Mani | Create new gomodule and use rsc.io/quote package

---
+++ day3/hello_world/hello_world.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"rsc.io/quote"
+)
+
+func main() {
+	fmt.Println("World travelers, useful phrase: ", quote.Glass())
+	fmt.Println("Go, Quote of the day: ", quote.Go())
+	fmt.Println("Greetings: ", quote.Hello())
+	fmt.Println("Optimization truth: ", quote.Opt())
+}
