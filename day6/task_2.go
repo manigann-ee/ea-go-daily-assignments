4b2dd42827430283c0e67adf1190e71746c19bad
Manigandan N <manigandan.n@everest.engineering>
2022-12-21 11:02:41 +0530

    #006 | Mani | Day6 assignment tasks

--- day6/task_2.go
+++ day6/task_2.go
@@ -2,16 +2,19 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 	"time"
 )
 
 func main() {
+	fmt.Println("GOMAXPROCS", runtime.GOMAXPROCS(1))
 	go random(5)
 	time.Sleep(1 * time.Second)
 	go random(10)
 	time.Sleep(1 * time.Second)
 	go random(15)
 	time.Sleep(1 * time.Second)
+	fmt.Println("GOMAXPROCS", runtime.GOMAXPROCS(1))
 }
 
 func random(loop int) {
