68ba2b804488d521e22ce01bb9e297effc3c48b7
Manigandan N <manigandan.n@everest.engineering>
2022-12-23 15:35:35 +0530

    #009 | Mani | Use gin recovery middleware

--- day9/main.go
+++ day9/main.go
@@ -8,10 +8,11 @@ import (
 func main() {
 	engine := gin.New()
 
+	engine.Use(gin.Recovery())
+
 	engine.POST("/api/songs", songs.CreateSongHandler)
 	engine.GET("/api/songs/:id", songs.GetSongHandler)
 	engine.GET("/api/songs", songs.GetSongsHandler)
 
 	engine.Run(":5050")
-
 }
